995696799cf5e54e53755e2c5e638b2c5bcb61d2
Hafeez Noor <hafeezn853@gmail.com>
2023-12-27 22:00:02 +0000

    initial commit

---
+++ app/src/main/java/com/example/onepass/activites/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.onepass.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.onepass.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
